750a616fa0c979af453da959c70b988c5d35be0f
sergeymaguire <sergeymaguire@gmail.com>
2019-03-06 14:50:34 -0800

    Done

--- demoOrAndAnd/Program.cs
+++ demoOrAndAnd/Program.cs
@@ -10,6 +10,29 @@ namespace demoOrAndAnd
     {
         static void Main(string[] args)
         {
+  
+            const int ZONE1 = 1, ZONE2 = 2;
+            const int LOW_QUANTITY = 10;
+            string inputString;
+            double fee = 25.75;
+            int quantity;
+            int deliveryZone;
+            Console.WriteLine("Delivery is free for zone {0} and zone {1} when the quanity ordered is less then {2} ", zone1, zone2, lowQuantity);
+            Console.Write("Please enter the delivery zone: ");
+            inputString = Console.ReadLine();
+            deliveryZone = Convert.ToInt32(inputString);
+            Console.Write("Please enter the quantity: ");
+            inputString = Console.ReadLine();
+            quantity = Convert.ToInt32(inputString);
+            if (deliveryZone == ZONE1 || deliveryZone == ZONE2)
+                if (quantity < LOW_QUANTITY)
+                {
+                    Console.WriteLine("The delivery is free");
+                }
+                else
+                {
+                    Console.WriteLine("There will be a charge of {0} ", fee);
+                }
         }
     }
 }
