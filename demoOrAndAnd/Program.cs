c96fee303748dd3e291cbb1b2ea83d9910334a1e
sergeymaguire <sergeymaguire@gmail.com>
2019-03-06 15:03:48 -0800

    refactor

--- demoOrAndAnd/Program.cs
+++ demoOrAndAnd/Program.cs
@@ -8,11 +8,12 @@ namespace demoOrAndAnd
 {
     class Program
     {
+        const int ZONE1 = 1, ZONE2 = 2;
+        const int LOW_QUANTITY = 10;
         static void Main(string[] args)
         {
-  
-            const int ZONE1 = 1, ZONE2 = 2;
-            const int LOW_QUANTITY = 10;
+
+
             string inputString;
             double fee = 25.75;
             int quantity;
@@ -24,17 +25,21 @@ namespace demoOrAndAnd
             Console.Write("Please enter the quantity: ");
             inputString = Console.ReadLine();
             quantity = Convert.ToInt32(inputString);
-            if (deliveryZone == ZONE1 || deliveryZone == ZONE2)
-                if (quantity < LOW_QUANTITY)
-                {
-                    Console.WriteLine("The delivery is free");
-                    Console.ReadLine();
-                }
-                else
-                {
-                    Console.WriteLine("There will be a charge of {0} ", fee);
-                    Console.ReadLine();
-                }
+            if (freeDelivery(deliveryZone, quantity))
+            {
+                Console.WriteLine("The delivery is free");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("There will be a charge of {0} ", fee);
+                Console.ReadLine();
+            }
+        }
+
+        private static bool freeDelivery(int deliveryZone, int quantity)
+        {
+            return ((deliveryZone == ZONE1 || deliveryZone == ZONE2) && (quantity < LOW_QUANTITY));  
         }
     }
 }
